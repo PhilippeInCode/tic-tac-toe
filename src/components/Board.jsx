dda291fcbdefbfececcb78d55db3b5b2102c5725
birdsinyourgarden <ruizpaloma9@gmail.com>
2025-01-20 10:58:22 +0100

    logic and components

---
+++ src/components/Board.jsx
@@ -0,0 +1,14 @@
+import Square from './Square';
+import '../components/Board.css';
+
+const Board = ({ squares, onSquareClick }) => {
+    return (
+        <div className="board">
+            {squares.map((value, index) => (
+                <Square key={index} value={value} onClick={() => onSquareClick(index)} />
+            ))}
+        </div>
+    );
+};
+
+export default Board;
